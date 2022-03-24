5cb92458a09e4982f695f5d9c2448352329da13a
Duveen <duveen@duveen.me>
2022-03-24 21:19:23 +0900

    Update install_iwinv.sh

--- install_iwinv.sh
+++ install_iwinv.sh
@@ -1,26 +1,23 @@
 #!/usr/bin/env bash
 
-BASE_REPOS=/etc/yum.repos.d/CentOS-Linux-BaseOS.repo
-
-KAKAO="mirror.kakao.com\/centos"
-
 if [ "$(id -u)" != "0" ]; then
    echo "'$0' must be run as root" 1>&2
    exit 1
 fi
 
-REPOS=${KAKAO}
-echo "Using Kakao repository(${REPOS})." >&2
+MIRROR="ftp.yjsoft.xyz\/rocky-linux\/8.5"
 
-releasever=$(cat /etc/centos-release | tr -dc '0-9.'|cut -d \. -f1)
-basearch=x86_64
+REPO=/etc/yum.repos.d/Rocky-AppStream.repo
+MOVE_REPOS="http:\/\/${MIRROR}\/AppStream\/x86_64\/os"
+sed  -i.bak -re "s/^(mirrorlist(.*))/##\1/g" -re "s/[#]*baseurl(.*)/baseurl=${MOVE_REPOS}/" ${REPO} 
 
-FULL_REPOS="http:\/\/${REPOS}\/${releasever}\/BaseOS\/${basearch}\/os"
+REPO=/etc/yum.repos.d/Rocky-BaseOS.repo
+MOVE_REPOS="http:\/\/${MIRROR}\/BaseOS\/x86_64\/os"
+sed  -i.bak -re "s/^(mirrorlist(.*))/##\1/g" -re "s/[#]*baseurl(.*)/baseurl=${MOVE_REPOS}/" ${REPO} 
 
-echo "using repository(${REPOS})"
-
-## change mirror
-sed  -i.bak -re "s/^(mirrorlist(.*))/##\1/g" -re "s/[#]*baseurl(.*)/baseurl=${FULL_REPOS}/" ${BASE_REPOS} 
+REPO=/etc/yum.repos.d/Rocky-Extras.repo
+MOVE_REPOS="http:\/\/${MIRROR}\/extras\/x86_64\/os"
+sed  -i.bak -re "s/^(mirrorlist(.*))/##\1/g" -re "s/[#]*baseurl(.*)/baseurl=${MOVE_REPOS}/" ${REPO}
 
 ## check
 dnf update -y
@@ -31,18 +28,13 @@ dnf install -y langpacks-en
 dnf groupinstall -y "Development Tools"
 dnf install -y git maven java-1.8.0-openjdk-devel
 
-
 echo "export LANG=en_US.utf8" >> /etc/environment
-echo "export JAVA_HOME=/etc/alternatives/java_jdk" >> /etc/environment
+echo "export JAVA_HOME=/etc/alternatives/java_sdk" >> /etc/environment
 
 systemctl enable --now chronyd
 chronyc tracking
 
 firewall-cmd --permanent --zone=public --add-rich-rule='rule family="ipv4" source address="222.236.125.250" port protocol="tcp" port="1-65535" accept'
-firewall-cmd --permanent --zone=public --add-rich-rule='rule family="ipv4" source address="10.6.1.67" port protocol="tcp" port="1-65535" accept'
-firewall-cmd --permanent --zone=public --add-rich-rule='rule family="ipv4" source address="10.6.1.139" port protocol="tcp" port="1-65535" accept'
-firewall-cmd --permanent --zone=public --add-rich-rule='rule family="ipv4" source address="0.0.0.0/0" port protocol="tcp" port="5070" accept'
-
 firewall-cmd --permanent --zone=public --remove-service=ssh
 firewall-cmd --permanent --zone=public --remove-service=cockpit
 
