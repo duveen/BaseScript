6a7a51e94fa49c2ddfff56946d9be5c864fb745a
Duveen <duveen@duveen.me>
2022-04-06 01:05:25 +0900

    Update install_intranet.sh

--- install_intranet.sh
+++ install_intranet.sh
@@ -21,6 +21,7 @@ sed  -i.bak -re "s/^(mirrorlist(.*))/##\1/g" -re "s/[#]*baseurl(.*)/baseurl=${MO
 
 wget https://raw.githubusercontent.com/duveen/BaseScript/main/epel-yjsoft -O /etc/yum.repos.d/epel.repo
 wget https://raw.githubusercontent.com/duveen/BaseScript/main/epel-modular-yjsoft -O /etc/yum.repos.d/epel-modular.repo
+wget http://ftp.yjsoft.xyz/epel/RPM-GPG-KEY-EPEL-8 -O /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-8
 
 ## check
 dnf update -y
