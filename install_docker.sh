cba731ba68876be07bbc6bd911d401c9c569e273
Duveen <duveen@duveen.me>
2022-04-05 18:35:50 +0900

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -4,4 +4,17 @@ sudo yum remove docker docker-client docker-client-latest docker-common docker-l
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install -y docker-ce docker-ce-cli containerd.io
+
+sudo mkdir /etc/docker
+sudo cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
 sudo systemctl start docker
