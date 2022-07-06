32cc6d9ddd7113a6195fc8f4c262ac86965bcce7
Duveen <duveen@duveen.me>
2022-07-06 15:40:33 +0900

    Update install_iwinv.sh

--- install_iwinv.sh
+++ install_iwinv.sh
@@ -5,20 +5,6 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-MIRROR="mirror.anigil.com\/rocky\/8.6"
-
-REPO=/etc/yum.repos.d/Rocky-AppStream.repo
-MOVE_REPOS="http:\/\/${MIRROR}\/AppStream\/x86_64\/os"
-sed  -i.bak -re "s/^(mirrorlist(.*))/##\1/g" -re "s/[#]*baseurl(.*)/baseurl=${MOVE_REPOS}/" ${REPO} 
-
-REPO=/etc/yum.repos.d/Rocky-BaseOS.repo
-MOVE_REPOS="http:\/\/${MIRROR}\/BaseOS\/x86_64\/os"
-sed  -i.bak -re "s/^(mirrorlist(.*))/##\1/g" -re "s/[#]*baseurl(.*)/baseurl=${MOVE_REPOS}/" ${REPO} 
-
-REPO=/etc/yum.repos.d/Rocky-Extras.repo
-MOVE_REPOS="http:\/\/${MIRROR}\/extras\/x86_64\/os"
-sed  -i.bak -re "s/^(mirrorlist(.*))/##\1/g" -re "s/[#]*baseurl(.*)/baseurl=${MOVE_REPOS}/" ${REPO}
-
 ## check
 dnf update -y
 
@@ -26,19 +12,11 @@ dnf install -y epel-release
 dnf install -y htop
 dnf install -y langpacks-en
 dnf groupinstall -y "Development Tools"
-dnf install -y java-1.8.0-openjdk-devel git maven gradle
+dnf install -y java-1.8.0-openjdk-devel git maven
 
 echo "export LANG=en_US.utf8" >> /etc/environment
 echo "export JAVA_HOME=/etc/alternatives/java_sdk" >> /etc/environment
 
 systemctl enable --now chronyd
+systemctl disable --now firewalld
 chronyc tracking
-
-firewall-cmd --permanent --zone=public --add-rich-rule='rule family="ipv4" source address="222.236.125.250" port protocol="tcp" port="1-65535" accept'
-firewall-cmd --permanent --zone=public --add-rich-rule='rule family="ipv4" source address="222.236.125.250" port protocol="udp" port="1-65535" accept'
-firewall-cmd --permanent --zone=public --add-rich-rule='rule family="ipv4" source address="10.7.0.0/16" port protocol="tcp" port="1-65535" accept'
-firewall-cmd --permanent --zone=public --add-rich-rule='rule family="ipv4" source address="10.7.0.0/16" port protocol="udp" port="1-65535" accept'
-firewall-cmd --permanent --zone=public --remove-service=ssh
-firewall-cmd --permanent --zone=public --remove-service=cockpit
-
-firewall-cmd --reload
